95200392455fb891160d2b64116d963fd22ba3e4
toshitai39 <112509719+toshitai39@users.noreply.github.com>
2022-08-30 23:56:48 +0530

    Add files via upload
    this file adds two number

---
+++ calculator.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+ 
+int main()
+{
+    int num1,num2;
+    float result;
+    char ope;    
+     
+    printf("Enter first number: ");
+    scanf("%d",&num1);
+    printf("Enter second number: ");
+    scanf("%d",&num2);
+     
+    printf("Choose operation to perform on the operands (+,-,*,/): ");
+    scanf(" %c",&ope);
+     
+    result=0;
+    switch(ope)    
+    {
+        case '+':
+            result=num1+num2;
+            break;
+             
+          default:
+            printf("Invalid operation.No such operation exsist");
+    }
+ 
+    printf("Result: %d %c %d = %f\n",num1,ope,num2,result);
+    return 0;
+}
