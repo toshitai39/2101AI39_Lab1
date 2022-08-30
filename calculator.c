f5a4ac3ba9fe53a46230642dfd091d318dddcd89
toshitai39 <112509719+toshitai39@users.noreply.github.com>
2022-08-31 00:02:12 +0530

    Update calculator.c
    Calculator

--- calculator.c
+++ calculator.c
@@ -23,6 +23,9 @@ int main()
       case '-':
             result=num1-num2;
             break;
+      case '*':
+            result=num1*num2;
+            break;
                     
           default:
             printf("Invalid operation.No such operation exsist");
